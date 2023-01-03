29f94494ecab5932a1a93c88a27467ac28d4ca3e
caduxl007 <eduardo2020dev@gmail.com>
2023-01-02 21:22:07 -0300

    feat: add page connect calendar

--- src/pages/register/hooks/useRegisterForm.tsx
+++ src/pages/register/hooks/useRegisterForm.tsx
@@ -1,23 +1,23 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { api } from '../../../lib/axios'
-import { AxiosError } from 'axios'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { api } from "../../../lib/axios";
+import { AxiosError } from "axios";
 
 const registerFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
-    .regex(/^([a-z\\-]+)$/i, { message: 'Apenas letras e hifens' })
+    .min(3, { message: "O usuário precisa ter pelo menos 3 letras" })
+    .regex(/^([a-z\\-]+)$/i, { message: "Apenas letras e hifens" })
     .transform((value) => value.toLowerCase()),
   name: z
     .string()
-    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
-})
+    .min(3, { message: "O nome precisa ter pelo menos 3 letras" }),
+});
 
-type RegisterFormData = z.infer<typeof registerFormSchema>
+type RegisterFormData = z.infer<typeof registerFormSchema>;
 
 export function useRegisterForm() {
   const {
@@ -27,36 +27,38 @@ export function useRegisterForm() {
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
-  })
+  });
 
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     if (router.query?.username) {
-      setValue('username', String(router.query.username))
+      setValue("username", String(router.query.username));
     }
-  }, [router.query?.username, setValue])
+  }, [router.query?.username, setValue]);
 
   async function handleRegister(data: RegisterFormData) {
     try {
-      await api.post('/users', {
+      await api.post("/users", {
         name: data.name,
-        username: data.username
-      })
+        username: data.username,
+      });
+
+      await router.push("/register/connect-calendar");
     } catch (err) {
-      if(err instanceof AxiosError && err?.response?.data?.message) {
-        alert(err?.response?.data?.message)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err?.response?.data?.message);
       }
 
-      console.error(err)
+      console.error(err);
     }
   }
- 
+
   return {
     register,
     handleSubmit,
     errors,
     isSubmitting,
-    handleRegister
-  }
+    handleRegister,
+  };
 }
