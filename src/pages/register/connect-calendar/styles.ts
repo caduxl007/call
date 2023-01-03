fb7db3c98467e65ac14402c99798ce13266a1f90
caduxl007 <eduardo2020dev@gmail.com>
2023-01-02 22:31:07 -0300

    feat: connect google calendar

--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react";
+import { Box, styled, Text } from "@ignite-ui/react";
 
 export const ConnectBox = styled(Box, {
   marginTop: "$6",
@@ -17,3 +17,8 @@ export const ConnectItem = styled("div", {
 
   marginBottom: "$2",
 });
+
+export const AuthError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4'
+})
