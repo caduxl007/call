940ece3561130acde0bdfe4f6f29980f0e821fda
caduxl007 <eduardo2020dev@gmail.com>
2023-01-04 20:40:37 -0300

    feat: create structure Calendar Step

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,31 @@
+import { Calendar } from "../../../../../components/Calendar";
+import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
+
+export function CalendarStep() {
+  const isDateSelected = true
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>terça-feira <span>20 de setembro</span></TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  );
+}
