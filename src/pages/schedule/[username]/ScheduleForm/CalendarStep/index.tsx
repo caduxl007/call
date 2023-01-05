047ede726534fab7111f663730814b97e45057fa
caduxl007 <eduardo2020dev@gmail.com>
2023-01-05 15:22:21 -0300

    refactor: remove usEffect get data and using react-query

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -18,7 +19,6 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [availability, setAvailability] = useState<Availability | null>(null);
 
   const router = useRouter();
 
@@ -30,21 +30,25 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format("DD[ de ] MMMM")
     : null;
 
-  useEffect(() => {
-    if (!selectedDate) {
-      return;
-    }
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format("YYYY-MM-DD")
+    : null;
 
-    api
-      .get(`/users/${username}/availability`, {
+  const { data: availability } = useQuery<Availability>(
+    ["availability", selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
         params: {
-          date: dayjs(selectedDate).format("YYYY-MM-DD"),
+          date: selectedDateWithoutTime,
         },
-      })
-      .then((response) => {
-        setAvailability(response.data);
       });
-  }, [selectedDate, username]);
+
+      return response.data;
+    },
+    {
+      enabled: !!selectedDate,
+    }
+  );
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
