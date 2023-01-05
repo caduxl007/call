4a394a625923b4ad33190589281dfd90740b35e8
caduxl007 <eduardo2020dev@gmail.com>
2023-01-05 20:17:03 -0300

    feat: blocked dates in past

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -31,6 +31,7 @@ interface CalendarProps {
 
 interface BlockedDates {
   blockedWeekDays: number[];
+  blockedDates: number[];
 }
 
 export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
@@ -64,7 +65,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get("year"),
-          month: currentDate.get("month"),
+          month: currentDate.get("month") + 1,
         },
       });
 
@@ -115,7 +116,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf("day").isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get("day")),
+            blockedDates?.blockedWeekDays.includes(date.get("day")) || blockedDates.blockedDates.includes(date.get('date')),
         };
       }),
       ...nextMonthFillArray.map((date) => {
