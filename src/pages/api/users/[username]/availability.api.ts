e1c011691f289843aa68303b6871f162a54fbe04
caduxl007 <eduardo2020dev@gmail.com>
2023-01-05 20:51:56 -0300

    fix: fix refence date end hour

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -64,7 +64,7 @@ export default async function handle(
       user_id: user.id,
       date: {
         gte: referenceDate.set("hour", startHour).toDate(),
-        lte: referenceDate.set("hour", startHour).toDate(),
+        lte: referenceDate.set("hour", endHour).toDate(),
       },
     },
   });
